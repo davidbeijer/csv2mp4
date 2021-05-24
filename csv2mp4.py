5d3f2ca31398f3ead6b71e28462b0517771daf6a
davidbeijer <david.beijer@gmail.com>
2021-05-24 16:48:41 +0200

    add comments and remove unused stuff

--- csv2mp4.py
+++ csv2mp4.py
@@ -1,27 +1,23 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import time
 
+##inital time, to measure performance
 start_time = time.time()
 
-
+##read the data from a CSV file, and account for excel's bullshit ; separator
 temps = pd.read_csv('dataCabine.csv', sep =";")
 
 
 title = 'Temperatures'
+
+##get the X and Y values from the pandas df
 y = np.array(temps.loc[:, "Temp1":"Temp1"])
 x = np.array(temps.loc[:, 'Sample':'Sample'])
-#temps.columns = {title}
 
-##initialize the writer
-#Writer = animation.writers['ffmpeg']
-#writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800)
-
-
+##initialize the layout of the plots
 fig, ax = plt.subplots()
 plt.xlim(0, np.max(x))
 plt.ylim(np.min(y), np.max(y))
@@ -29,16 +25,18 @@ plt.xlabel('time',fontsize=20)
 plt.ylabel(title,fontsize=20)
 plt.title('Measured temperature',fontsize=20)
 
+##initialize the first data-line
 line, = ax.plot(x, y, color='b', label='Temp1')
 
-
+## what needs to update in the animation?
 def animate(i):
     line.set_data(x[:i], y[:i])  
-    
-    
-    
+
+## make the animation    
 ani = matplotlib.animation.FuncAnimation(fig, animate, interval=10, frames=len(x)-1)
 
+##write the animation
 ani.save('result.mp4', dpi=300)
 
+#print how long it all took
 print("--- %s seconds ---" % (time.time() - start_time))
